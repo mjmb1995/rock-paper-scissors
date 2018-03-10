fa159395338a57b3420c6022b640566b11207cd6
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2018-03-10 15:09:06 -0800

    Hides score vars from global scope

--- assets/app.js
+++ assets/app.js
@@ -1,58 +1,79 @@
 "use strict";
 
-// initial scores
-var wins = 0;
-var losses = 0;
-var draws = 0;
+var playerWon = updateWin();
+var playerLost = updateLosses();
+var tied = updateDraws();
 
-// valid choices
-var validChoices = ["r", "p", "s"];
-var userInput = "";
-var computerGuess = "";
+// hiding the score variables
+function updateWin() {
+  var wins = 0;
 
-
-//computer selects from array of options
-function computerChoice(){
-	var rand = Math.floor((Math.random() * validChoices.length - 1) + 1);
-	computerGuess = validChoices[rand];
+  return function () {
+    wins++;
+    document.getElementById("won").innerHTML = wins;
+  }
 }
 
-//updates per match results
-function displayResults(){
-	document.getElementById("playerChoice").innerHTML = userInput;
-	document.getElementById("computer").innerHTML = computerGuess;
-	document.getElementById("wins").innerHTML = wins;
-	document.getElementById("losses").innerHTML = losses;
-	document.getElementById("draws").innerHTML = draws;
+function updateLosses() {
+  var losses = 0;
+
+  return function () {
+    losses++;
+
+    document.getElementById("loss").innerHTML = losses;
+  }
+}
+
+function updateDraws() {
+  var draws = 0;
+
+  return function () {
+    draws++;
+    document.getElementById("draw").innerHTML = draws;
+  }
 }
 
 // game starts with user pressing the appropriate key
 document.onkeyup = function(event){
-	userInput = event.key;
 
+  var userInput = event.key.toLowerCase();
+  
+  
 	if ((userInput === "r") || (userInput === "p") || (userInput === "s")) {
-		computerChoice();
-        if ((userInput === "r") && (computerGuess === "s")) {
-          wins++
-        }
-        else if ((userInput === "r") && (computerGuess === "p")) {
-          losses++
-        }
-        else if ((userInput === "s") && (computerGuess === "r")) {
-          losses++
-        }
-        else if ((userInput === "s") && (computerGuess === "p")) {
-          wins++
-        }
-        else if ((userInput === "p") && (computerGuess === "r")) {
-          wins++
-        }
-        else if ((userInput === "p") && (computerGuess === "s")) {
-          losses++
-        }
-        else if (userInput === computerGuess) {
-          draws++
-        }
-        displayResults();
+		var computerGuess = computerChoice();
+
+    if ((userInput === "r") && (computerGuess === "s")) {
+      playerWon();
     }
+    else if ((userInput === "r") && (computerGuess === "p")) {
+      playerLost();
+    }
+    else if ((userInput === "s") && (computerGuess === "r")) {
+      playerLost();
+    }
+    else if ((userInput === "s") && (computerGuess === "p")) {
+      playerWon();
+    }
+    else if ((userInput === "p") && (computerGuess === "r")) {
+      playerWon();
+    }
+    else if ((userInput === "p") && (computerGuess === "s")) {
+      playerLost();
+    }
+    else{
+      tied();
+    }
+
+  document.getElementById("playerChoice").innerHTML = userInput;
+  document.getElementById("computer").innerHTML = computerGuess;
+
+  }
+}
+
+//computer selects from array of options
+function computerChoice(){
+  // valid choices
+  var validChoices = ["r", "p", "s"];
+  var rand = Math.floor((Math.random() * validChoices.length - 1) + 1);
+  return validChoices[rand];
 }
